d016d3f171dee27aa151d1c780c9834223b6ae63
Vassil <vassiln@yahoo.com>
2018-01-16 00:12:30 +0200

    Added some checks and warning messages in cases when some of the fields is not filled in.

--- src/filemodifier/FileModifierController.java
+++ src/filemodifier/FileModifierController.java
@@ -5,6 +5,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
@@ -24,16 +26,35 @@ public class FileModifierController {
     @FXML
     private TextField replacementText;
 
-    private Stage fileOpener;
-
     public void handleRunAction() {
         if (isAllInformationPresent()) {
-            FileModifier.modifyFiles(filePath.getText(), fileName.getText(), regexPattern.getText(), replacementText.getText());
+            FileModifier.modifyFiles(filePath.getText().trim(), fileName.getText().trim(), regexPattern.getText().trim(), replacementText.getText().trim());
+        } else {
+            setWarningMessages();
+        }
+    }
+
+    private void setWarningMessages() {
+        if (filePath.getText().equals("")) {
+            filePath.setText("Please enter a valid path!");
+            filePath.setStyle("-fx-text-fill: red");
+        }
+        if (fileName.getText().equals("")) {
+            fileName.setText("Please enter a file name to look for!");
+            fileName.setStyle("-fx-text-fill: red");
+        }
+        if (regexPattern.getText().equals("")) {
+            regexPattern.setText("Please enter a text pattern to look for!");
+            regexPattern.setStyle("-fx-text-fill: red");
+        }
+        if (replacementText.getText().equals("")) {
+            replacementText.setText("Pleace enter a replacement text!");
+            replacementText.setStyle("-fx-text-fill: red");
         }
     }
 
     private boolean isAllInformationPresent() {
-        return filePath.getText() != null && fileName.getText() != null && regexPattern.getText() != null && replacementText.getText() != null;
+        return !filePath.getText().equals("") && !fileName.getText().equals("") && !regexPattern.getText().equals("") && !replacementText.getText().equals("");
     }
 
     public void handleQuitAction(ActionEvent event) {
@@ -41,7 +62,7 @@ public class FileModifierController {
     }
 
     public void handlePickPathAction(ActionEvent event) {
-        fileOpener= new Stage();
+        Stage fileOpener = new Stage();
         fileOpener.setTitle("Choose path to file");
         fileOpener.initModality(Modality.APPLICATION_MODAL);
 
@@ -54,5 +75,22 @@ public class FileModifierController {
         if (file != null) {
             filePath.setText(file.getAbsolutePath());
         }
+    }
+
+    public void resetTextStyle(MouseEvent event) {
+        if (fileName.getStyle().equals("-fx-text-fill: red")) {
+            removeTextFiieldStyling();
+        }
+    }
+
+    private void removeTextFiieldStyling() {
+        fileName.setStyle(null);
+        fileName.clear();
+        filePath.setStyle(null);
+        filePath.clear();
+        regexPattern.setStyle(null);
+        regexPattern.clear();
+        replacementText.setStyle(null);
+        replacementText.clear();
     }
 }
